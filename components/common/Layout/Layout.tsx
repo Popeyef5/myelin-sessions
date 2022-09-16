d646e49c06a0d142ae8b6cfe7190402133308989
Gaston Maffei <gastonmaffei@gmail.com>
2022-09-16 19:08:58 -0300

    meta

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -9,6 +9,9 @@ export const Layout = ({ children }: FlexProps) => {
       <Head>
         <title>Myelin Sessions</title>
         <meta name="description" content="Intimate encounters about the future with top founders and investors" />
+        <meta property="og:title" content="short title of your website/webpage" />
+        <meta property="og:url" content="https://sessions.myelin.vc/" />
+        <meta property="og:description" content="Intimate encounters about the future with top founders and investors" />
       </Head>
       <Box flexGrow="1">
         <main>{children}</main>
